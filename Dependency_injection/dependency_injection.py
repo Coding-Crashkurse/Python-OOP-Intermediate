f8832ff41ef6f64e8fff6cd58f9f0f9bc4b74509
markus <datamastery87@gmail.com>
2023-02-02 18:59:03 +0100

    add dependecy injection code

---
+++ Dependency_injection/dependency_injection.py
@@ -0,0 +1,74 @@
+# Schlechter Code
+
+
+class Motor:
+    def start(self):
+        print("Motor gestartet")
+
+
+class Auto:
+    def __init__(self):
+        self.motor = Motor()
+
+    def start(self):
+        self.motor.start()
+        print("Auto gestartet")
+
+
+auto = Auto()
+auto.start()
+
+
+# Guter Code
+class Motor:
+    def start(self):
+        print("Motor started")
+
+
+class Auto:
+    def __init__(self, motor):
+        self.motor = motor
+
+    def start(self):
+        self.motor.start()
+        print("Auto started")
+
+
+motor = Motor()
+auto = Auto(motor)
+auto.start()
+
+from abc import ABC, abstractmethod
+
+class BaseEngine(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Verbrennungsmotor(BaseEngine):
+    def start(self):
+        print("Verbrennungsmotor gestartet")
+
+
+class Elektromotor(BaseEngine):
+    def start(self):
+        print("Elektromotor gestartet")
+
+
+class Auto:
+    def __init__(self, motor: BaseEngine):
+        self.motor = motor
+
+    def start(self):
+        self.motor.start()
+        print("Auto gestartet")
+
+
+motor = Verbrennungsmotor()
+auto = Auto(motor)
+auto.start()
+
+motor = Elektromotor()
+auto = Auto(motor)
+auto.start()
