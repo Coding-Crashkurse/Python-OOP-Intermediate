91856f2597a3bc03bd392d990a8b9f7c52ad74be
markus <datamastery87@gmail.com>
2023-02-05 11:29:02 +0100

    add abstract factory

---
+++ AbstractFactory/abstractfactory.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+class BaseMotor(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+class Verbrennungsmotor(BaseMotor):
+    def start(self):
+        print("Verbrennungsmotor gestartet")
+
+class ElektroMotor(BaseMotor):
+    def start(self):
+        print("Elektromotor gestartet")
+
+class Auto:
+    def __init__(self, motor: BaseMotor):
+        self.motor = motor
+
+    def start(self):
+        print("Auto wurde gestartet")
+        self.motor.start()
+
+class EngineFactory(ABC):
+    @abstractmethod
+    def create_engine(self):
+        pass
+
+
+class VerbrennungsmotorEngineFactory(EngineFactory):
+    def create_engine(self):
+        return Verbrennungsmotor()
+
+
+class ElektromotorEngineFactory(EngineFactory):
+    def create_engine(self):
+        return ElektroMotor()
+
+engine_factory = ElektromotorEngineFactory()
+motor = engine_factory.create_engine()
+auto = Auto(motor)
+auto.start()
+
+engine_factory = VerbrennungsmotorEngineFactory()
+motor = engine_factory.create_engine()
+auto = Auto(motor)
+auto.start()
