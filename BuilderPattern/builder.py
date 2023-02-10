b0f16546fb5a2324d037d7f2794db24afe566b63
markus <datamastery87@gmail.com>
2023-02-10 17:29:36 +0100

    builder pattern

--- BuilderPattern/builder.py
+++ BuilderPattern/builder.py
@@ -1,54 +1,35 @@
-class Engine:
-    def __init__(self, horsepower: int, num_cylinders: int):
-        self.horsepower = horsepower
-        self.num_cylinders = num_cylinders
-
-    def __str__(self):
-        return f"Engine with {self.horsepower} horsepower and {self.num_cylinders} cylinders."
-
 class Car:
-    def __init__(self, make: str, model: str, engine: Engine):
-        self.make = make
+    def __init__(self, brand: str, model: str, color: str):
+        self.brand = brand
         self.model = model
-        self.engine = engine
-
-    def __str__(self):
-        return f"{self.make} {self.model} with {self.engine}"
+        self.color = color
 
 class CarBuilder:
     def __init__(self):
-        self.make = ""
+        self.brand = ""
         self.model = ""
-        self.horsepower = 0
-        self.num_cylinders = 0
+        self.color = "red"
 
-    def set_make(self, make: str):
-        self.make = make
+    def set_brand(self, brand: str):
+        self.brand = brand
         return self
 
     def set_model(self, model: str):
         self.model = model
         return self
 
-    def set_horsepower(self, horsepower: int):
-        self.horsepower = horsepower
-        return self
-
-    def set_num_cylinders(self, num_cylinders: int):
-        self.num_cylinders = num_cylinders
+    def set_color(self, color: str):
+        self.color = color
         return self
 
     def build(self):
         return Car(
-            make=self.make,
+            brand=self.brand,
             model=self.model,
-            engine=Engine(
-                horsepower=self.horsepower,
-                num_cylinders=self.num_cylinders
-            )
+            color=self.color
         )
 
-builder = CarBuilder().set_make("Ford").set_model("Mustang").set_horsepower(400).set_num_cylinders(8)
+builder = CarBuilder().set_brand("Ford").set_model("Mustang").set_color("green")
+print(builder.__dict__)
 car = builder.build()
-print(car)
-# Output: Ford Mustang with Engine with 400 horsepower and 8 cylinders.
+print(car.__dict__)
